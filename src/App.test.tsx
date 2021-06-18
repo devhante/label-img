e01481dc16325ac4d5cfa513e8a258d9426e6cc9
Kyungwook Cho <devhante@gmail.com>
2021-06-18 23:41:01 +0900

    Add Header component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import Header from 'components/Header';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('render header', () => {
+  const { getByText } = render(<Header />);
+  const titleElement = getByText(/Dataset Label/i);
+  expect(titleElement).toBeInTheDocument();
+});
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="title">Dataset Label</div>
+    </div>
+  );
+}
+
+export default Header;
